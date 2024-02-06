1a9bb7be7261e2e90e1d84cf5d96f5a80a1e53fe
biersdorfer-jon <jbiersdorfer02@gmail.com>
2024-02-06 00:35:42 -0600

    ico

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -9,7 +9,7 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 800px;
+    height: 856px;
     align-items: start;
     opacity: 0;
     justify-content: start;
